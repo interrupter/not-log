99c9f0002dbe1d24339d900e7cc896e543498e9f
Aleksander Repin <alex.repin@mail.ru>
2019-01-12 19:45:14 +0300

    v0.0.14

--- src/lib.js
+++ src/lib.js
@@ -7,10 +7,12 @@
 * @const {module} winston Winston logger module
 */
 const winston = require('winston');
+
 /**
 * @const {string} ENV value of process.env.NODE_ENV
 */
 const ENV = process.env.NODE_ENV;
+
 /**
  * Returns logger for module
  * @param {cjsmodule|string} input  if module object then used to localize error file
