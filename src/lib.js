cf57c21f2f9410ff0987e35c97924994a2e05af8
Aleksander Repin <alex.repin@mail.ru>
2020-08-20 19:17:58 +0300

    simple logger, with filename and label; methods: log, debug, error

--- src/lib.js
+++ src/lib.js
@@ -1,44 +1,64 @@
-/**
- * Simple wrapper for logging
- * @module not-log
- */
+const path = require('path');
+const DEV_ENV = 'production';
+const ENV_TYPE = process.env.NODE_ENV ? process.env.NODE_ENV : DEV_ENV;
+const NOOP = () => {};
 
-/**
- * @const {module} winston Winston logger module
- */
-const {
-	createLogger,
-	format,
-	transports
-} = require('winston');
-const {
-	combine,
-	timestamp,
-	prettyPrint,
-	label
-} = format;
-/**
- * @const {string} ENV value of process.env.NODE_ENV
- */
-const ENV = process.env.NODE_ENV;
-/**
- * Returns logger for module
- * @param {cjsmodule|string} input  if module object then used to localize error file
- * if string, then it changes pathToLog
- * @return {object} Winston instance
- */
-function getLogger(module) {
-	return createLogger({
-		format: combine(
-			label({
-				filename: module.filename,
-				ENV
-			}),
-			timestamp(),
-			prettyPrint()
-		),
-		transports: [new transports.Console()]
-	});
+function pad(n) {
+	return n < 10 ? '0' + n : n;
 }
 
-module.exports = getLogger;
+function localIsoDate(date) {
+	date = date || new Date;
+	let localIsoString = date.getFullYear() + '-' +
+		pad(date.getMonth() + 1) + '-' +
+		pad(date.getDate()) + 'T' +
+		pad(date.getHours()) + ':' +
+		pad(date.getMinutes()) + ':' +
+		pad(date.getSeconds());
+	return localIsoString;
+}
+
+//Генерация метода вывода сообщений в консоль с указанием префикса.
+function genLogMsg(prefix) {
+	return function(){
+		let now = localIsoDate();
+		// eslint-disable-next-line no-console
+		console.log(`[${now}]: ${prefix}::`, ...arguments);
+	};
+}
+
+function isDev() {
+	return ENV_TYPE === DEV_ENV;
+}
+
+function genLogDebug(prefix) {
+	if (isDev()) {
+		return genLogMsg(prefix);
+	} else {
+		return NOOP;
+	}
+}
+
+function genLogError(prefix) {
+	return function(){
+		let now = localIsoDate();
+		// eslint-disable-next-line no-console
+		console.error(`[${now}]: ${prefix}::`, ...arguments);
+	};
+}
+
+function simpleLogger(module, label = ''){
+	let parts = path.parse(module.__filename);
+	let breadcrumbs = [parts.base];
+	if(label && label.length){
+		breadcrumbs.unshift(label);
+	}
+	breadcrumbs = breadcrumbs.join(' : ');
+	return {
+		log: genLogMsg(breadcrumbs),
+		debug: genLogDebug(breadcrumbs),
+		error: genLogError(breadcrumbs),
+	};
+}
+
+module.exports = simpleLogger;
